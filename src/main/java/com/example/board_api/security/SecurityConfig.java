0a612fdafcad7beff1a5dfa686547c5c0414488d
LeeJunSoo <leejeunsoo@clouflake.com>
2024-07-23 21:02:38 +0900

    feat: 회원가입 xml 오류 수정

--- src/main/java/com/example/board_api/security/SecurityConfig.java
+++ src/main/java/com/example/board_api/security/SecurityConfig.java
@@ -15,8 +15,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-@Configuration
-@EnableWebSecurity // 필터 체인 관리 시작 어노테이션
+@Configuration  //스프링의 구성 클래스임을 의미 <- 덕분에 스프링이 자동 인식 후 설정 적용
+@EnableWebSecurity // 웹 보안 활성화 + 스프링 시큐리티 설정 클래스임을 의미
 public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -30,12 +30,11 @@ public class SecurityConfig {
 //                .csrf(AbstractHttpConfigurer::disable)
 //                .cors(AbstractHttpConfigurer::disable);
         http
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())   //cors 설정 활성화
-//                .authorizeRequests(auth -> auth
-//                        .anyRequest().authenticated()
-//                );
-                .authorizeRequests(authorize -> authorize.requestMatchers("/**").permitAll() // 모든 요청 허용
+                .csrf(csrf -> csrf.disable())   //csrf 비활성화
+                .cors(cors -> cors.disable())   //cors 비활성화
+                .authorizeRequests(authorize -> authorize
+                        .requestMatchers("/**").permitAll() // 요청 허용할 경로 명시(현재는 모든 경로)
+//                        .anyRequest().authenticated() // 밑의 permitAll()과 반대로 인증할 경로 명시(인증된 사용자만 접근 허용)
         );
 
         return http.build();
@@ -44,18 +43,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        //쿠키 허용 여부
-        config.setAllowCredentials(true);
-        //허용할 도메인
-        config.setAllowedOrigins(Arrays.asList("http://localhost:9000"));
-        //허용할 HTTP 메서드
-        config.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE"));
-        //허용할 헤더
-        config.setAllowedHeaders(Arrays.asList("*"));
-
+        config.setAllowCredentials(true);   //쿠키 허용 여부
+        config.setAllowedOrigins(Arrays.asList("http://localhost:9000"));   //허용할 도메인
+        config.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE"));  //허용할 HTTP 메서드
+        config.setAllowedHeaders(Arrays.asList("*"));   //허용할 헤더
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        // 모든 경로에 대해 CORS 설정을 적용.
-        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration("/**", config);    // 모든 경로에 대해 CORS 설정을 적용.
         return source;
     }
 }
